be392322847d0a4563b24cf5649b3177c72d7d71
Kali <tavarescauanf@gmail.com>
2024-02-27 17:23:26 -0300

    Desafio de criacao das rotas update e delete da categories

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -10,6 +10,22 @@ class CategoryController{
 
   }
 
+  async show(req, res){
+
+    // Obter 1 registro
+    const { id } = req.params
+    const category = await CategoryRepository.findById(id)
+
+    if (!category){
+
+      return res.status(404).json({ error: 'Category not found' })
+
+    }
+
+    res.json(category)
+
+  }
+
   async store(req, res){
 
     const { name } = req.body
@@ -19,6 +35,42 @@ class CategoryController{
     const category = await CategoryRepository.create(name)
 
     res.json(category)
+
+  }
+
+  async update(req, res){
+
+    // Atualiza novo registro
+    const { id } = req.params
+    const {
+      name,
+    } = req.body
+
+    const categoryExists = await CategoryRepository.findById(id)
+
+    if (!categoryExists) return res.status(400).json({ error: 'Category not found' })
+
+    const categoryByName = await CategoryRepository.findByName(name)
+
+    if (categoryByName && categoryByName.id !== id) return res.status(400).json({ error: 'This name is alredy in use' })
+
+    const category = await CategoryRepository.update(
+      id,
+      name,
+    )
+
+    res.json(category)
+
+  }
+
+  async delete(req, res){
+
+    // Deleta novo registro
+    const { id } = req.params
+
+    await CategoryRepository.delete(id)
+
+    res.sendStatus(204) // OK: No content
 
   }
 
--- src/app/repositories/CategoryRepository.js
+++ src/app/repositories/CategoryRepository.js
@@ -13,6 +13,28 @@ class CategoryRepository{
 
   }
 
+  async findById(id){
+
+    const [row] = await db.query(`
+        SELECT * FROM categories
+        WHERE id = $1
+    `, [id])
+
+    return row
+
+  }
+
+  async findByName(name){
+
+    const [row] = await db.query(`
+        SELECT * FROM categories
+        WHERE name = $1
+    `, [name])
+
+    return row
+
+  }
+
   async create(name){
 
     const [row] = await db.query(`
@@ -22,6 +44,30 @@ class CategoryRepository{
     `, [name])
 
     return row
+
+  }
+
+  async update(id, name){
+
+    const [row] = await db.query(`
+        UPDATE categories
+        SET name = $1
+        WHERE id = $2
+        RETURNING *
+    `, [name, id])
+
+    return row
+
+  }
+
+  async delete(id){
+
+    const deleteOp = await db.query(`
+        DELETE FROM categories
+        WHERE id = $1
+    `, [id])
+
+    return deleteOp
 
   }
 
